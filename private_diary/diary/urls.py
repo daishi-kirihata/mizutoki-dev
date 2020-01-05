4e4c9f151ffca0a960cfec8c90674834a321d6c6
d.kirihata <hajime.jizo@gmail.com>
2020-01-06 01:56:26 +0900

    フォーム画面の追加

--- private_diary/diary/urls.py
+++ private_diary/diary/urls.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jan  3 04:48:17 2020
-
-@author: hajime
-"""
-
 
 from django.urls import path
+
 from . import views
 
 app_name = 'diary'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
 ]
