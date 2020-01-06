affe2b395df589dee01a16dc71ca807c5c0630cb
d.kirihata <hajime.jizo@gmail.com>
2020-01-07 03:04:59 +0900

    メール送信機能機能の追加

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from logging import getLogger
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
-
+from django.contrib import messages
 from .forms import InquiryForm
+
+logger = getLogger(__name__)
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -10,3 +14,10 @@ class IndexView(TemplateView):
 class InquiryView(FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+    
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
